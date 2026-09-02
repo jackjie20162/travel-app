42f45e662abc13eba02fce0ed409c6d449539197
jackjie20162 <610841147@qq.com>
2026-09-02 15:00:08 +0800

    chore: move rpc server to dedicated repository

--- internal/server/server.go
+++
@@ -1,8 +0,0 @@
-package server
-
-import "github.com/zeromicro/go-zero/zrpc"
-
-// Register reserves the RPC registration point for generated protobuf services.
-// The initial repository keeps the contract and service boundary explicit;
-// generated registration code will be added after protoc/gen-rpc is wired into CI.
-func Register(_ *zrpc.RpcServer) {}
